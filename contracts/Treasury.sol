14fb83065145b719152f425bc086cbfe80da4c6b
chaudhryasim5 <chaudhryasim5@gmail.com>
2023-08-04 03:30:20 +0500

    update

---
+++ contracts/Treasury.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IUniswapV2Router02 {
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+}
+
+interface IAaveLendingPool {
+    function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
+    function withdraw(address asset, uint256 amount, address to) external returns (uint256);
+}
+
+contract Treasury is Ownable {
+    using SafeERC20 for IERC20;
+
+    IUniswapV2Router02 public uniswapRouter;
+    IAaveLendingPool public aaveLendingPool;
+
+    IERC20 public usdcToken;
+    IERC20 public usdtToken;
+    IERC20 public daiToken;
+
+    uint256 public uniswapRatio;
+    uint256 public aaveRatio;
+
+    constructor(
+        address _uniswapRouter,
+        address _aaveLendingPool,
+        address _usdcToken,
+        address _usdtToken,
+        address _daiToken
+    ) {
+        uniswapRouter = IUniswapV2Router02(_uniswapRouter);
+        aaveLendingPool = IAaveLendingPool(_aaveLendingPool);
+        usdcToken = IERC20(_usdcToken);
+        usdtToken = IERC20(_usdtToken);
+        daiToken = IERC20(_daiToken);
+    }
+
+    function setRatios(uint256 _uniswapRatio, uint256 _aaveRatio) external onlyOwner {
+        require(_uniswapRatio + _aaveRatio == 100, "Ratios must add up to 100");
+        uniswapRatio = _uniswapRatio;
+        aaveRatio = _aaveRatio;
+    }
+
+    function deposit(uint256 amount) external {
+        usdcToken.safeTransferFrom(msg.sender, address(this), amount);
+
+        uint256 usdtAmount = amount * uniswapRatio / 100;
+        uint256 daiAmount = amount * aaveRatio / 100;
+
+        address[] memory path = new address[](2);
+        path[0] = address(usdcToken);
+        path[1] = address(usdtToken);
+
+        usdcToken.approve(address(uniswapRouter), usdtAmount);
+        uniswapRouter.swapExactTokensForTokens(usdtAmount, 0, path, address(this), block.timestamp);
+
+        usdcToken.approve(address(aaveLendingPool), daiAmount);
+        aaveLendingPool.deposit(address(usdcToken), daiAmount, address(this), 0);
+    }
+
+    function withdraw(uint256 amount) external onlyOwner {
+        uint256 usdtAmount = amount * uniswapRatio / 100;
+        uint256 daiAmount = amount * aaveRatio / 100;
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = usdtAmount;
+        amounts[1] = daiAmount;
+
+        aaveLendingPool.withdraw(address(usdcToken), daiAmount, address(this));
+        uniswapRouter.swapExactTokensForTokens(usdtAmount, 0, getPathForTokens(address(usdtToken)), address(this), block.timestamp);
+
+        usdtToken.transfer(owner(), usdtAmount);
+        daiToken.transfer(owner(), daiAmount);
+    }
+
+    function calculateYield() external view returns (uint256) {
+        uint256 usdtBalance = usdtToken.balanceOf(address(this));
+        uint256 daiBalance = daiToken.balanceOf(address(this));
+
+        return usdtBalance + daiBalance;
+    }
+
+    function getPathForTokens(address token) private view returns (address[] memory) {
+        address[] memory path = new address[](2);
+        path[0] = address(token);
+        path[1] = address(usdcToken);
+
+        return path;
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const uniswapRouterAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+  const aaveLendingPoolAddress = "0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf";
+  const usdcTokenAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+  const usdtTokenAddress = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
+  const daiTokenAddress = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
+
+  console.log("TEstinbg")
+
+  const treasury = await ethers.deployContract("Treasury", [
+    uniswapRouterAddress,
+    aaveLendingPoolAddress,
+    usdcTokenAddress,
+    usdtTokenAddress,
+    daiTokenAddress,
+  ]);
+
+  await treasury.waitForDeployment();
+  let treasury_address = await treasury.getAddress();
+
+  console.log("Treasury deployed to:", treasury_address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
